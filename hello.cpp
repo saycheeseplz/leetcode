39041b2b335edd3f9d0a25de02f09aa23d4df776
saycheeseplz <ledinhquan2006www@gmail.com>
2025-08-11 18:17:16 +0700

    hello

---
+++ hello.cpp
@@ -0,0 +1,21 @@
+/*
+Author : @wuan
+Link :
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define pi pair<ll, ll>
+#define fi first
+#define se second
+#define endl '\n'
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    cout<<"hello"<<endl;
+    return 0;
+}
