5637d7399f642cb567673c4036b817bec50e848f
saycheeseplz <ledinhquan2006www@gmail.com>
2025-08-13 15:34:15 +0700

    space

--- test.cpp
+++ test.cpp
@@ -1,31 +1,3 @@
-<<<<<<< HEAD
-class Solution
-{
-public:
-    int findDuplicate(vector<int> &nums)
-    {
-        int slow = nums[0];
-        int fast = nums[0];
-
-        // Bước 1: Gặp nhau trong chu kỳ
-        do
-        {
-            slow = nums[slow];
-            fast = nums[nums[fast]];
-        } while (slow != fast);
-
-        // Bước 2: Tìm điểm bắt đầu chu kỳ (chính là số bị lặp)
-        slow = nums[0];
-        while (slow != fast)
-        {
-            slow = nums[slow];
-            fast = nums[fast];
-        }
-
-        return slow;
-    }
-};
-=======
 /*
 Author : @wuan
 Link :
@@ -70,4 +42,3 @@ int main()
     cout << getHint("1807", "7810");
     return 0;
 }
->>>>>>> memo
