0628f0241ce990eaeb41f784f48d1f01cafc09dc
saycheeseplz <ledinhquan2006www@gmail.com>
2025-08-13 15:31:28 +0700

    fix conflict

--- test.cpp
+++ test.cpp
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 class Solution
 {
 public:
@@ -25,49 +24,3 @@ public:
         return slow;
     }
 };
-=======
-/*
-Author : @wuan
-Link :
-*/
-
-#include <bits/stdc++.h>
-using namespace std;
-#define ll long long
-#define pi pair<ll, ll>
-#define fi first
-#define se second
-#define endl '\n'
-string getHint(string secret, string guess)
-{
-    int pull = 0;
-    int cow = 0;
-    int a[10] = {0};
-    for (int i = 0; i < secret.size(); i++)
-    {
-        a[secret[i] - '0']++;
-        if (secret[i] == guess[i])
-        {
-            pull++;
-        }
-    }
-    for (int i = 0; i < guess.size(); i++)
-    {
-        if (a[guess[i] - '0'] > 0)
-        {
-            cow++;
-            a[guess[i] - '0']--;
-        }
-    }
-    string s = to_string(pull) + "A" + to_string(cow) + "B";
-    return s;
-}
-int main()
-{
-    ios_base::sync_with_stdio(0);
-    cin.tie(NULL);
-    cout.tie(NULL);
-    cout << getHint("1807", "7810");
-    return 0;
-}
->>>>>>> memo
