6679ceb813a681f985c6520f6442592d9f500250
saycheeseplz <ledinhquan2006www@gmail.com>
2025-08-17 22:22:34 +0700

    repeat

--- bitch.cpp
+++ bitch.cpp
@@ -1,39 +1,125 @@
+/*
+Author : @wuan
+Link :
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define pi pair<ll, ll>
+#define fi first
+#define se second
+#define endl '\n'
 class Solution
 {
 public:
-    bool validUtf8(vector<int> &data)
+    string repeat(int n, string s)
     {
-        int remaining = 0; // số continuation byte còn cần
-
-        for (int num : data)
+        string word = s;
+        for (int i = 1; i < n; i++)
         {
-            if (remaining == 0)
+            word += s;
+        }
+        return word;
+    }
+    string decodeString(string s)
+    {
+        string word = "";
+        for (int i = 0; i < s.size(); i++)
+        {
+            string word1;
+            int res = 1;
+            if (isdigit(s[i]))
             {
-                // Đếm số bit 1 liên tiếp từ trái
-                int mask = 0b10000000;
-                int count = 0;
-                while (mask & num)
+                res = 0;
+                while (isdigit(s[i]))
                 {
-                    count++;
-                    mask >>= 1;
+                    res = res * 10 + (s[i] - '0');
+                    i++;
                 }
-
-                // Byte đầu hợp lệ: 0, 2, 3, 4 byte; 1 hoặc >4 là sai
-                if (count == 1 || count > 4)
-                    return false;
-
-                remaining = count == 0 ? 0 : count - 1;
+            }
+            if (s[i] == '[')
+            {
+                int depth = 0;
+                int j = i + 1;
+                while (!(depth == 0 && s[j] == ']'))
+                {
+                    if (s[j] == '[')
+                    {
+                        depth++;
+                    }
+                    else if (s[j] == ']')
+                    {
+                        depth--;
+                    }
+                    j++;
+                }
+                word1 = decodeString(s.substr(i + 1, j - i - 1));
+                i = j;
             }
             else
             {
-                // Byte continuation phải bắt đầu bằng 10
-                if ((num & 0b11000000) != 0b10000000)
-                    return false;
-                remaining--;
+                word1 += s[i];
+            }
+            word += repeat(res, word1);
+        }
+        return word;
+    }
+};
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    Solution b;
+    cout << b.decodeString("3[a]2[bc]");
+    return 0;
+}
+// prenium
+class Solution
+{
+public:
+    string decodeString(string s)
+    {
+        stack<int> counts;         // lưu số lần repeat
+        stack<string> resultStack; // lưu chuỗi trước dấu [
+        string current = "";
+        int k = 0;
+
+        for (char c : s)
+        {
+            if (isdigit(c))
+            {
+                k = k * 10 + (c - '0'); // tính số nhiều chữ số
+            }
+            else if (c == '[')
+            {
+                counts.push(k);
+                resultStack.push(current);
+                current = "";
+                k = 0;
+            }
+            else if (c == ']')
+            {
+                int repeatTimes = counts.top();
+                counts.pop();
+                string prev = resultStack.top();
+                resultStack.pop();
+
+                // tối ưu nối string bằng reserve
+                string repeated;
+                repeated.reserve(current.size() * repeatTimes);
+                for (int i = 0; i < repeatTimes; i++)
+                    repeated += current;
+
+                current = prev + repeated;
+            }
+            else
+            {
+                current += c; // ký tự bình thường
             }
         }
 
-        // Sau khi duyệt hết, không còn continuation byte bị thiếu
-        return remaining == 0;
+        return current;
     }
 };
