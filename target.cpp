716cbaaf4478c2c616d81fcc8bcb142f6e4d99ec
saycheeseplz <ledinhquan2006www@gmail.com>
2025-08-21 15:41:03 +0700

    alatic and ocean

--- target.cpp
+++ target.cpp
@@ -1,28 +1,64 @@
 class Solution
 {
 public:
-    bool canPartition(vector<int> &nums)
+    vector<int> dx{-1, 1, 0, 0};
+    vector<int> dy{0, 0, -1, 1};
+
+    void bfs(vector<vector<int>> &height, int environment, vector<vector<int>> &visited, queue<pair<int, int>> &evi, int n, int m)
     {
-        int sum = 0;
-        for (int num : nums)
-            sum += num;
-
-        // Nếu tổng lẻ -> không thể chia thành 2 phần bằng nhau
-        if (sum % 2 != 0)
-            return false;
-
-        int target = sum / 2;
-        vector<bool> dp(target + 1, false);
-        dp[0] = true;
-
-        for (int num : nums)
+        while (!evi.empty())
         {
-            for (int j = target; j >= num; --j)
+            int na = evi.size();
+            for (int i = 0; i < na; i++)
             {
-                dp[j] = dp[j] || dp[j - num];
+                pair<int, int> top = evi.front();
+                evi.pop();
+                for (int k = 0; k < 4; k++)
+                {
+                    int nx = top.first + dx[k];
+                    int ny = top.second + dy[k];
+                    if (nx >= 0 && ny >= 0 && nx < n && ny < m && height[nx][ny] >= height[top.first][top.second] && !(visited[nx][ny] & environment))
+                    {
+                        visited[nx][ny] |= environment;
+                        evi.push({nx, ny});
+                    }
+                }
             }
         }
-
-        return dp[target];
+    }
+    vector<vector<int>> pacificAtlantic(vector<vector<int>> &heights)
+    {
+        int n = heights.size();
+        int m = heights[0].size();
+        vector<vector<int>> visited(n, vector<int>(m, 0));
+        queue<pair<int, int>> atlantic, ocean;
+        for (int i = 0; i < n; i++)
+        {
+            atlantic.push({i, 0});
+            visited[i][0] |= 1;
+            ocean.push({i, m - 1});
+            visited[i][m - 1] |= 2;
+        }
+        for (int i = 0; i < m; i++)
+        {
+            visited[0][i] |= 1;
+            visited[n - 1][i] |= 2;
+            atlantic.push({0, i});
+            ocean.push({n - 1, i});
+        }
+        bfs(heights, 1, visited, atlantic, n, m);
+        bfs(heights, 2, visited, ocean, n, m);
+        vector<vector<int>> result;
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < m; j++)
+            {
+                if (visited[i][j] == 3)
+                {
+                    result.push_back({i, j});
+                }
+            }
+        }
+        return result;
     }
 };
