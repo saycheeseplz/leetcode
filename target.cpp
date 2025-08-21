f13e6be66485d31558f0e49db35a794e00b21173
saycheeseplz <ledinhquan2006www@gmail.com>
2025-08-21 13:30:12 +0700

    quy hoach dong

---
+++ target.cpp
@@ -0,0 +1,28 @@
+class Solution
+{
+public:
+    bool canPartition(vector<int> &nums)
+    {
+        int sum = 0;
+        for (int num : nums)
+            sum += num;
+
+        // Nếu tổng lẻ -> không thể chia thành 2 phần bằng nhau
+        if (sum % 2 != 0)
+            return false;
+
+        int target = sum / 2;
+        vector<bool> dp(target + 1, false);
+        dp[0] = true;
+
+        for (int num : nums)
+        {
+            for (int j = target; j >= num; --j)
+            {
+                dp[j] = dp[j] || dp[j - num];
+            }
+        }
+
+        return dp[target];
+    }
+};
