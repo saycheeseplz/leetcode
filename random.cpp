1327da293be9bea6c612b46b4b7eb8093ae856d4
saycheeseplz <ledinhquan2006www@gmail.com>
2025-09-05 14:17:42 +0700

    chia de chi chon diem xet tuwngf doan do dai

--- random.cpp
+++ random.cpp
@@ -1,59 +1,24 @@
-/*
-Author : @wuan
-Link :
-*/
-
-#include <bits/stdc++.h>
-using namespace std;
-#define ll long long
-#define pi pair<ll, ll>
-#define fi first
-#define se second
-#define endl '\n'
 class Solution
 {
 public:
-    bool predict(vector<int> &nums, int start, int end, int total, int turn, unordered_map<int, bool> mp)
+    int dfs(const vector<int> &nums, int l, int r, vector<vector<int>> &memo)
     {
-        int x = start * 1000 + end;
-        if (mp.count(x))
-        {
-            return mp[x];
-        }
-        if (start == end)
-        {
-            total += turn * nums[start];
-            bool check = total >= 0;
-            check = turn == 1 ? check : !check;
-
-            return check;
-        }
-
-        bool check = (predict(nums, start + 1, end, total + nums[start] * turn, turn * -1, mp) |
-                      predict(nums, start, end - 1, total + nums[end] * turn, turn * -1, mp));
-        check = turn == 1 ? check : !check;
-        if (check)
-        {
-            return mp[x] = true;
-        }
-        return mp[x] = false;
+        if (l == r)
+            return nums[l];
+        if (memo[l][r] != INT_MIN)
+            return memo[l][r];
+        int pickLeft = nums[l] - dfs(nums, l + 1, r, memo);
+        int pickRight = nums[r] - dfs(nums, l, r - 1, memo);
+        memo[l][r] = max(pickLeft, pickRight);
+        return memo[l][r];
     }
 
     bool predictTheWinner(vector<int> &nums)
     {
-        unordered_map<int, bool> mp;
-        int n = nums.size() - 1;
-        return predict(nums, 0, n, 0, 1, mp);
+        int n = nums.size();
+        if (n <= 1)
+            return true;
+        vector<vector<int>> memo(n, vector<int>(n, INT_MIN));
+        return dfs(nums, 0, n - 1, memo) >= 0;
     }
 };
-int main()
-{
-    ios_base::sync_with_stdio(0);
-    cin.tie(NULL);
-    cout.tie(NULL);
-
-    vector<int> nums({1, 5, 2});
-    Solution b;
-    cout << b.predictTheWinner(nums);
-    return 0;
-}
