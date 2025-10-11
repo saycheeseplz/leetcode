60b286a57c88e6c3dca61fdaed5d36cceced108f
saycheeseplz <ledinhquan2006www@gmail.com>
2025-10-11 16:29:18 +0700

    coin

--- yc.cpp
+++ yc.cpp
@@ -77,3 +77,22 @@ int main()
     }
     return 0;
 }
+// pro bo nho o(n)
+int longestPalindromeSubseq(string s)
+{
+    int n = s.size();
+    vector<int> dp(n, 0), prev(n, 0);
+    for (int i = n - 1; i >= 0; i--)
+    {
+        dp[i] = 1;
+        for (int j = i + 1; j < n; j++)
+        {
+            if (s[i] == s[j])
+                dp[j] = prev[j - 1] + 2;
+            else
+                dp[j] = max(prev[j], dp[j - 1]);
+        }
+        prev = dp;
+    }
+    return dp[n - 1];
+}
