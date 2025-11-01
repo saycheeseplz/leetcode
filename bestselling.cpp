03e79a785d57d2d85121b77102c463882b69cb4c
saycheeseplz <ledinhquan2006www@gmail.com>
2025-11-01 09:07:33 +0700

    time carr

--- bestselling.cpp
+++ bestselling.cpp
@@ -1,49 +1,41 @@
 class Solution
 {
 public:
-    pair<int, int> split(string s, bool &isEnd)
+    static bool cmd(pair<int, int> a, pair<int, int> b)
     {
-        int n = s.size();
-        int firstColon = s.find(':');
-        int secondColon = s.find(':', firstColon + 1);
-
-        int id = stoi(s.substr(0, firstColon));
-        string type = s.substr(firstColon + 1, secondColon - firstColon - 1);
-        int time = stoi(s.substr(secondColon + 1));
-
-        isEnd = (type == "end");
-        return {id, time};
-    }
-
-    vector<int> exclusiveTime(int n, vector<string> &logs)
-    {
-        vector<int> result(n, 0);
-        stack<int> st; // lưu ID hàm đang chạy
-        int prevTime = 0;
-
-        for (string s : logs)
+        if (a.first >= b.first)
         {
-            bool isEnd;
-            auto [id, time] = split(s, isEnd);
-
-            if (!isEnd) // start
+            return true;
+        }
+        return false;
+    }
+    int carFleet(int target, vector<int> &position, vector<int> &speed)
+    {
+        vector<pair<int, int>> flag;
+        int n = position.size();
+        for (int i = 0; i < n; i++)
+        {
+            flat.push_back({position[i], speed[i]});
+        }
+        sort(flat.begin(), flat.end(), cmd);
+        stack<int> st;
+        for (auto temp : flat)
+        {
+            int distance = temp.first;
+            int sd = temp.second;
+            int time = (target - distance) / sd;
+            if (st.empty())
             {
-                if (!st.empty())
-                {
-                    // cộng thời gian cho hàm hiện tại trước khi bị tạm dừng
-                    result[st.top()] += time - prevTime;
-                }
-                st.push(id);
-                prevTime = time; // cập nhật lại thời điểm bắt đầu
+                st.push(time);
             }
-            else // end
+            else
             {
-                result[st.top()] += time - prevTime + 1; // +1 vì end tính cả timestamp hiện tại
-                st.pop();
-                prevTime = time + 1; // cập nhật cho hàm cha
+                if (st.top() < time)
+                {
+                    st.push(time);
+                }
             }
         }
-
-        return result;
+        return st.size();
     }
 };
