3b8311e5b13665f4e2208286472141c706bc356f
saycheeseplz <ledinhquan2006www@gmail.com>
2025-11-02 15:48:55 +0700

    deque  reverse logic

---
+++ aheach.cpp
@@ -0,0 +1,20 @@
+class Solution
+{
+public:
+    vector<int> deckRevealedIncreasing(vector<int> &deck)
+    {
+        sort(deck.begin(), deck.end());
+        int n = deck.size();
+        deque<int> dq;
+        for (int n - 1; i >= 0; i--)
+        {
+            if (!dq.empty())
+            {
+                dp.push_front(dp.back());
+                dp.pop_back();
+            }
+            dp.push_front(deck[i]);
+        }
+        return vector<int>(dq.begin(), dq.end());
+    }
+};
