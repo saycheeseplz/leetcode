cf8648eef1dfec5278c16af2b5d9f63a68ac9b4b
saycheeseplz <ledinhquan2006www@gmail.com>
2025-11-03 20:30:55 +0700

    so lon nhat tren doan

---
+++ hate.cpp
@@ -0,0 +1,28 @@
+vector<int> maxSlidingWindow(vector<int> &nums, int k)
+{
+    deque<int> dq;
+    vector<int> res;
+
+    for (int i = 0; i < nums.size(); i++)
+    {
+        // pop out-of-window index
+        while (!dq.empty() && dq.front() <= i - k)
+        {
+            dq.pop_front();
+        }
+
+        // pop smaller elements (never useful)
+        while (!dq.empty() && nums[i] >= nums[dq.back()])
+        {
+            dq.pop_back();
+        }
+
+        dq.push_back(i);
+
+        if (i >= k - 1)
+        {
+            res.push_back(nums[dq.front()]);
+        }
+    }
+    return res;
+}
